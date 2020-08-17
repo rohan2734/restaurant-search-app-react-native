7c5f518879779aceae2b2af536ce01cce18ff734
rohan2734 <jagandevaki1@gmail.com>
2020-08-17 14:32:30 +0530

    adding styles to searchBar

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -3,12 +3,19 @@ import {View,Text,StyleSheet} from 'react-native';
 
 const SearchBar = () => {
   return(
-    <View>
+    <View style={styles.background}>
       <Text>Search Bar</Text>
     </View>
   )
 };
 
-const styles= StyleSheet.create({});
+const styles= StyleSheet.create({
+  background:{
+    backgroundColor:'#C3C3C3',
+    height:50,
+    borderRadius:5,
+    marginHorizontal:15
+  }
+});
 
 export default SearchBar;
