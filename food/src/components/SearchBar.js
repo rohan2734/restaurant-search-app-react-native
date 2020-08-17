89c92672e29186c1d04122c2007b93abc8cecaf9
rohan2734 <jagandevaki1@gmail.com>
2020-08-17 14:18:03 +0530

    basic architecture

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from  'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+const SearchBar = () => {
+  return(
+    <View>
+      <Text>Search Bar</Text>
+    </View>
+  )
+};
+
+const styles= StyleSheet.create({});
+
+export default SearchBar;
