ba3754dd83510ed9c89ab33e1368b03e64293632
rohan2734 <jagandevaki1@gmail.com>
2020-08-18 15:18:23 +0530

    added modification to resultList

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,17 +1,29 @@
 import React from  'react';
-import {View,Text,StyleSheet,FlatList} from 'react-native';
+import {View,Text,StyleSheet,FlatList,TouchableOpacity} from 'react-native';
 import ResultsDetail from './ResultsDetail'
+import {withNavigation} from 'react-navigation';
 
-const ResultsList = ({title,results}) => {
+const ResultsList = ({title,results,navigation}) => {
+  if(!results.length){
+    return null
+  }
+  
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
       <FlatList 
         horizontal
+        showsHorizontalScrollIndicator={false}
         data={results}
         keyExtractor={(result) => result.id}
         renderItem={({item}) => {
-          return <ResultsDetail result={item} />
+          return (
+            <TouchableOpacity 
+              onPress={() => navigation.navigate('ResultsShow',{id:item.id})}
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          )
         }}
       />
     </View>
@@ -29,4 +41,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from  'react';
-import {View,Text,StyleSheet} from 'react-native';
+import {View,Text,StyleSheet,ScrollView} from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
@@ -16,18 +16,29 @@ const SearchScreen = () => {
   }
   
   return(
-    <View >
+    <>
       <SearchBar 
         term={term} 
         onTermChange={setTerm} 
         onTermSubmit={()=>searchApi(term)} 
       />
       {errorMessage ?  <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
-      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"/>
-      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricer" />
-      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
-    </View>
+      {/* <Text>We have found {results.length} results</Text> */}
+      <ScrollView>
+        <ResultsList 
+          results={filterResultsByPrice('$')} 
+          title="Cost Effective"
+        />
+        <ResultsList 
+          results={filterResultsByPrice('$$')} 
+          title="Bit Pricer" 
+        />
+        <ResultsList 
+          results={filterResultsByPrice('$$$')} 
+          title="Big Spender" 
+        />
+      </ScrollView>
+    </>
   )
 };
 
